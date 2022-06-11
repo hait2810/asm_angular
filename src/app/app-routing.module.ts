ead004acbf99932e02203498b824cb3fac429fb0
Hai <haibong2001@gmail.com>
2022-06-11 21:43:16 +0700

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WorksComponent } from './components/works/works.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: "", component: WorksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
